f84248ab975e2a76cd8de8d108d37db9af8e72fa
skysys <975273494@qq.com>
2022-07-11 16:08:29 +0800

    信息安全实验提交版本

---
+++ pages/_app.js
@@ -0,0 +1,32 @@
+import '../styles/globals.css'
+import Link from 'next/link'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">
+          <img src="/logo.png" width="140px" height="38px" style={{display: 'inline-block', marginRight: '20px'}}/>
+          Metaverse Marketplace
+        </p>
+        <div className="flex mt-4">
+          <Link href="/">
+            <a className="mr-4 text-pink-500">市场</a>
+          </Link>
+          <Link href="/create-nft">
+            <a className="mr-6 text-pink-500">发布</a>
+          </Link>
+          <Link href="/dashboard">
+            <a className="mr-6 text-pink-500">看板</a>
+          </Link>
+          <Link href="/my-nfts">
+            <a className="mr-6 text-pink-500">我的</a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
+}
+
+export default MyApp
